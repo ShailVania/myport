4e8b1688ec963a2c3358ff39f933e4ab6e1e18e8
ShailVania <vanias@uwindsor.ca>
2023-10-17 22:22:37 -0400

    Created a ContactForm

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,26 @@
 import "./index.scss"
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 const Contact = () => {
     const [letterClass,setLetterClass] = useState('text-animate')
+    const refForm = useRef()
 
     useEffect(() => {
         setTimeout(()=>{
             setLetterClass('text-animate-hover')
         }, 3000)
     }, []);
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        // emailjs{
+        //
+        // }
+
+    }
     return(
         <>
             <div className='container contact-page'>
@@ -26,6 +36,27 @@ const Contact = () => {
                         Let's collaborate on your next project.
                         Reach out to me, and let's create some digital magic together!
                     </p>
+                    <div className='contact-form'>
+                        <form ref={refForm} onSubmit={sendEmail}>
+                            <ul>
+                                <li className='half'>
+                                    <input type="text" name="name" placeholder='Name' required={true}/>
+                                </li>
+                                <li className='half'>
+                                    <input type="email" name="email" placeholder='E-mail' required={true}/>
+                                </li>
+                                <li>
+                                    <input placeholder='Subject' type='text' name='subject' required={true}/>
+                                </li>
+                                <li>
+                                    <textarea placeholder='Message' name='message' required={true}></textarea>
+                                </li>
+                                <li>
+                                    <input type='submit' className='flat-button' value="SEND"/>
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
                 </div>
 
             </div>
